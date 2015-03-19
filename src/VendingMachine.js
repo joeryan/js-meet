75a5d9f68a7d301ee0d1980251a31aac72cb1a92
Joe Ryan <joe@clanryan.us>
2015-03-19 06:49:06 -0400

    refactor coin deposit functionality

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,14 +1,13 @@
 var VendingMachine = {
   create: function() {
       _display = "INSERT COIN";
+      _coins = {"NICKEL": "0.05", "DIME": "0.10", "QUARTER": "0.25"}
       function display() {
           return _display;
       }
 
       function insertCoin(coin) {
-	  if (coin === "NICKEL") _display = "0.05";
-	  if (coin === "DIME") _display = "0.10";
-	  if (coin === "QUARTER") _display = "0.25";
+	  _display = _coins[coin];
       }
 
       return {
